4ed4d1d027a930660a7350a79a614fb52385bedd
Riquelme <riquelmeoliveira85@gmail.com>
2026-09-25 09:58:40 -0300

    Corrections

--- 03_loops_solved/exercise_01.c
+++ 03_loops_solved/exercise_01.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 int main()
 {
+    
     float inicial, aumento, final;
-    int ano_inicial, ano_atual;
+    int ano_atual;
     
     inicial = 1000;
     aumento = 1.5 / 100;
     ano_atual = 2026;
     final = inicial + (aumento * inicial);
 
-    for(int ano_inicial = 2007;ano_inicial < ano_atual; ano_inicial++)
+    for(int ano_inicial = 2007;ano_inicial <= ano_atual; ano_inicial++)
     {
         aumento *= 2;
         final = final + aumento * final;
